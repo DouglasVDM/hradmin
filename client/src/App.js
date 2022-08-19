b7c343f8161b66ba21c616d9f4b38f344d44e642
Douglas <douglas@sentian.co.za>
2022-08-19 05:39:49 +0200

    import the inputdepartment

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,17 @@
 import { Fragment } from 'react';
 import './App.css';
 
+// components
+import InputDepartment from './components/InputDepartment';
+
+
 function App() {
 	return (
-		<Fragment></Fragment>
+		<Fragment>
+			<div className="container">
+				<InputDepartment />
+			</div>
+		</Fragment>
 	);
 
 }
