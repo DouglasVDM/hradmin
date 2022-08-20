9d677c42ea6d1bcad317e084a0dd632c8d23fcb3
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-08-20 23:26:21 +0200

    add display all the departments

--- client/src/components/ListDepartments.jsx
+++ client/src/components/ListDepartments.jsx
@@ -1,58 +1,54 @@
 import React, { Fragment, useState } from 'react';
+import { useEffect } from 'react';
 
-const ListDepartment = () => {
+const ListDepartments = () => {
+	const [departments, setDepartments] = useState(
+		[]
+	);
 
-const [description, setDescription] = useState('');
+	const getDepartments = async () => {
+		try {
+			const response = await fetch(
+				"http://localhost:5000/departments"
+			);
+			const jsonData = await response.json();
+			setDepartments(jsonData);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
 
-const onSubmitForm = async (event)=>{
-	event.preventDefault();
-
-	try {
-		const body = {description};
-		const response = await fetch("http://localhost:5000/departments", {
-			method: "GET",
-			headers: {"Content-Type":"application/json"},
-			body: JSON.stringify(body)
-		});
-
-		console.log(response);
-
-	} catch (err) {
-		console.error(err.message)
-	}
-}
+	useEffect(() => {
+		getDepartments();
+	}, []);
 
 	return (
 		<Fragment>
-			<h1 className='text-center mt-5'>List Department</h1>
-			<table class="table table-hover mt-5">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-</Fragment>
-	)
-}
+			<h1 className="text-center mt-5">
+				List Department
+			</h1>
+			<table className="table table-hover mt-5">
+				<thead>
+					<tr>
+						<th>Description</th>
+						<th>Edit</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{departments.map(
+						(department, index) => (
+							<tr key={index}>
+								<td>{department.description}</td>
+								<td>Edit</td>
+								<td>Delete</td>
+							</tr>
+						)
+					)}
+				</tbody>
+			</table>
+		</Fragment>
+	);
+};
 
-export default ListDepartment;
+export default ListDepartments;
