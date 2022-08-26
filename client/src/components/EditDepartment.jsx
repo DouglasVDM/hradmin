8dcc09e9900a009cbbb0dcb185f75d0fa004505e
Douglas <douglas@sentian.co.za>
2022-08-26 06:52:57 +0200

    add edit modal markup

--- client/src/components/EditDepartment.jsx
+++ client/src/components/EditDepartment.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment } from 'react';
 
-const EditDepartment = props => {
+const EditDepartment = () => {
 	return (
-		<div>EditDepartment</div>
-	)
-}
+		<Fragment>
+			<button
+				type="button"
+				className="btn btn-warning"
+				data-bs-toggle="modal"
+				data-bs-target="#exampleModal"
+			>
+				Edit
+			</button>
+			<div
+				className="modal fade"
+				id="exampleModal"
+				tabIndex="-1"
+				aria-labelledby="exampleModalLabel"
+				aria-hidden="true"
+			>
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5
+								className="modal-title"
+								id="exampleModalLabel"
+							>
+								Edit Department
+							</h5>
+							<button
+								type="button"
+								className="btn-close"
+								data-bs-dismiss="modal"
+								aria-label="Close"
+							></button>
+						</div>
+						<div className="modal-body">
+							<input
+								type="text"
+								className="form-control"
+							/>
+						</div>
+						<div className="modal-footer">
+							<button
+								type="button"
+								className="btn btn-warning"
+								data-bs-dismiss="modal"
+							>
+								Save changes
+							</button>
+							<button
+								type="button"
+								className="btn btn-secondary"
+								data-bs-dismiss="modal"
+							>
+								Exit
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>{' '}
+		</Fragment>
+	);
+};
 
-EditDepartment.propTypes = {}
-
-export default EditDepartment
+export default EditDepartment;
