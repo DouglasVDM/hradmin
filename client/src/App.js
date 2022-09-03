b18d288365aba3806ec2365e70dd36a960ca1a18
Douglas <douglas@sentian.co.za>
2022-09-03 23:08:41 +0200

    send state to children via props

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import './App.css';
 
 // components
@@ -8,11 +7,33 @@ import ListDepartment from './components/ListDepartments';
 
 
 function App() {
+
+	const [departments, setDepartments] = useState([]);
+
+	const getDepartments = async () => {
+		try {
+			const response = await fetch(
+				'http://localhost:5000/departments'
+			);
+			const jsonData = await response.json();
+
+			setDepartments(jsonData);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
+	useEffect(() => {
+		getDepartments();
+	}, []);
+
+	console.log(departments);
+
 	return (
 		<Fragment>
 			<div className="container">
-				<InputDepartment />
-				<ListDepartment />
+				<InputDepartment departments={departments} setDepartments={setDepartments} />
+				<ListDepartment departments={departments} setDepartments={setDepartments} />
 			</div>
 		</Fragment>
 	);
