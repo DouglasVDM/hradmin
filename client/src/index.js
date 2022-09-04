d297ef0acbc619f92ba44862a152932b3fb1f874
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-09-04 23:20:20 +0200

    remove react redux provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// redux imports
-import { Provider } from "react-redux";
-import store from './store';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
 
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
